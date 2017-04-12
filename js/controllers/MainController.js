8d4cdcf5d609d2df8b2ca5d72a760b0216c30ff7
MaxCCT <maxcct@cantab.net>
2017-04-12 13:30:55 -0600

    Lays more foundation

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,48 @@
 app.controller('MainController', ['$scope', function($scope) {
 	$scope.sections = [
+		{
+			conventional: {
+				title: 'Conventional Education',
+				text: 'placeholder'
+			},
+			honest: {
+				title: 'Honest Education',
+				text: 'placeholder'
+			},
+			braggy: {
+				title: 'Braggy Education',
+				text: 'placeholder' 
+			}
+		},
 
+		{
+			conventional: {
+				title: 'Conventional Experience',
+				text: 'placeholder'
+			},
+			honest: {
+				title: 'Honest Experience',
+				text: 'placeholder'
+			},
+			braggy: {
+				title: 'Braggy Experience',
+				text: 'placeholder' 
+			}
+		},
+
+		{
+			conventional: {
+				title: 'Conventional Personal',
+				text: 'placeholder'
+			},
+			honest: {
+				title: 'Honest Personal',
+				text: 'placeholder'
+			},
+			braggy: {
+				title: 'Braggy Personal',
+				text: 'placeholder' 
+			}
+		}
 	];
 }]);
